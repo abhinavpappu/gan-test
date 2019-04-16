2a9ca6ae2c86134e9eb9e9b318ad0edcca0da9bd
Abhinav Pappu <abhi1pappu9@gmail.com>
2019-04-16 12:49:18 -0500

    Complete training function, but still have errors

--- main.py
+++ main.py
@@ -60,7 +60,10 @@ class DiscriminatorNet(torch.nn.Module):
         )
     
     def forward(self, x):
+        print('d')
+        print(type(x))
         x = self.hidden0(x),
+        print(type(x))
         x = self.hidden1(x),
         x = self.hidden2(x),
         x = self.out(x)
@@ -82,7 +85,7 @@ class GeneratorNet(torch.nn.Module):
     def __init__(self):
         super(GeneratorNet, self).__init__()
 
-        n_features = 100
+        n_features = 100 # i think this is arbitrary
         n_out = 784
 
         self.hidden0 = nn.Sequential(
@@ -106,7 +109,10 @@ class GeneratorNet(torch.nn.Module):
         )
 
     def forward(self, x):
+        print('g')
+        print(type(x))
         x = self.hidden0(x)
+        print(type(x))
         x = self.hidden1(x)
         x = self.hidden2(x)
         x = self.out(x)
@@ -140,7 +146,7 @@ def zeros_target(size):
 
 
 def train_discriminator(optimizer, real_data, fake_data):
-    N = real_data.size(0)
+    N = real_data.size(0) # batch size
 
     # Reset gradients
     optimizer.zero_grad()
@@ -169,4 +175,71 @@ def train_discriminator(optimizer, real_data, fake_data):
     # Return error and predictions for real and fake inputs
     return error_real + error_fake, prediction_real, prediction_fake
 
+
+def train_generator(optimizer, fake_data):
+    N = fake_data.size(0) # batch size
+
+    # Reset gradients
+    optimizer.zero_grad()
+
+    # Sample noise and generate fake data
+    # See what the discriminator thinks of the fake data
+    prediction = discriminator(fake_data)
+
+    # Calculate error and backpropagate
+    # We want the discriminator to output ones (it thinks the data is real)
+    error = loss(prediction, ones_target(N))
+    error.backward()
+
+    # Update weights with gradients
+    optimizer.step()
+
+    return error
+
+# We want to visualize how the training process develops as the GAN learns
+num_test_samples = 16
+test_noise = noise(num_test_samples)
+
+
+# Create Logger instance
+logger = Logger(model_name='VGAN', data_name='MNIST')
+
+# Total number of epochs to train
+num_epochs = 200
+
+for epoch in range(num_epochs):
+    for n_batch, (real_batch, _) in enumerate(data_loader):
+        N = real_batch.size(0)
+
+        real_data = Variable(images_to_vectors(real_batch))
+
+        # 1. Train Discriminator
+
+        # Generate fake data and detach (so gradients are not calculated for the generator)
+        fake_data = generator(noise(N))
+
+        # Train discriminator
+        d_error, d_pred_real, d_pred_fake = train_discriminator(d_optimizer, real_data, fake_data)
+
+
+        # 2. Train Generator
+
+        # Generate fake data (not detaching this time since we do want the gradients calculated to train)
+        fake_data = generator(noise(N))
+
+        # Train generator
+        g_error = train_generator(g_optimizer, fake_data)
+
+
+        # Log batch error
+        logger.log(d_error, g_error, epoch, n_batch, num_batches)
+
+        # Display progress every few batches
+        if (n_batch) % 100 == 0:
+            test_images = vectors_to_images(generator(test_noise)).data
+
+            logger.log_images(test_images, num_test_samples, epoch, n_batch, num_batches)
+
+            # Display status logs
+            logger.display_status(epoch, num_epochs, n_batch, num_batches, d_error, g_error, d_pred_real, d_pred_fake)
 
